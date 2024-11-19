5a32fe06e883b7e188957d6b63c9e530454c3ced
romander333 <123271232+romander333@users.noreply.github.com>
2024-11-19 10:33:51 +0200

    Update OOP.cpp

--- OOP.cpp
+++ OOP.cpp
@@ -1,21 +1,21 @@
 #include <iostream>
 #include <vector>
 
-// Шаблонна функція для сортування методом перестановок
+
 template <typename T>
 void permutationSort(std::vector<T>& arr) {
     size_t n = arr.size();
     for (size_t i = 0; i < n - 1; ++i) {
         for (size_t j = i + 1; j < n; ++j) {
             if (arr[i] > arr[j]) {
-                std::swap(arr[i], arr[j]); // Перестановка елементів
+                std::swap(arr[i], arr[j]); 
             }
         }
     }
 }
 
 int main() {
-    // Приклад використання з цілими числами
+  
     std::vector<int> numbers = { 5, 2, 9, 1, 5, 6 };
 
     std::cout << "Before sorting: ";
@@ -24,7 +24,7 @@ int main() {
     }
     std::cout << std::endl;
 
-    // Виклик шаблонної функції
+   
     permutationSort(numbers);
 
     std::cout << "after sorting: ";
@@ -33,7 +33,7 @@ int main() {
     }
     std::cout << std::endl;
 
-    // Приклад з рядками
+   
     std::vector<std::string> words = { "apple", "orange", "banana", "pear" };
 
     std::cout << "Before sorting: ";
