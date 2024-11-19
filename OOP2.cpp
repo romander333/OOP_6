4fb85e44c157eb7c5b041bb020840d858e159348
romander333 <123271232+romander333@users.noreply.github.com>
2024-11-19 10:34:30 +0200

    Update OOP2.cpp

--- OOP2.cpp
+++ OOP2.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 #include <type_traits>
 
-// Шаблонний клас для обчислення квадрату елементів масиву
+
 template <typename T>
 class ArrayTransformer {
     static_assert(std::is_arithmetic<T>::value, "The type must be numeric (int, float, long etc)");
@@ -11,17 +11,17 @@ private:
     std::vector<T> data;
 
 public:
-    // Конструктор для ініціалізації масиву
+   
     explicit ArrayTransformer(const std::vector<T>& input) : data(input) {}
 
-    // Метод для перетворення масиву
+  
     void transformToSquares() {
         for (auto& element : data) {
-            element *= element; // Квадрат елемента
+            element *= element;
         }
     }
 
-    // Метод для виводу масиву
+   
     void print() const {
         for (const auto& element : data) {
             std::cout << element << " ";
@@ -31,7 +31,7 @@ public:
 };
 
 int main() {
-    // Масив типу int
+   
     std::vector<int> intArray = { 1, 2, 3, 4, 5 };
     ArrayTransformer<int> intTransformer(intArray);
 
@@ -43,7 +43,7 @@ int main() {
     std::cout << "Array int before conversion: ";
     intTransformer.print();
 
-    // Масив типу float
+   
     std::vector<float> floatArray = { 1.1, 2.2, 3.3, 4.4, 5.5 };
     ArrayTransformer<float> floatTransformer(floatArray);
 
@@ -55,7 +55,7 @@ int main() {
     std::cout << "Array float after conversion:";
     floatTransformer.print();
 
-    // Масив типу long
+  
     std::vector<long> longArray = { 10, 20, 30, 40, 50 };
     ArrayTransformer<long> longTransformer(longArray);
 
